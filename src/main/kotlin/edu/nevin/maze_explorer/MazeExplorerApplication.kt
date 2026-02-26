3dc71aef92feaf868997749f194235a2cea00dd5
Nevin Fullerton <nfullerton@carroll.edu>
2026-02-26 11:13:47 -0700

    init

---
+++ src/main/kotlin/edu/nevin/maze_explorer/MazeExplorerApplication.kt
@@ -0,0 +1,11 @@
+package edu.nevin.maze_explorer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MazeExplorerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MazeExplorerApplication>(*args)
+}
